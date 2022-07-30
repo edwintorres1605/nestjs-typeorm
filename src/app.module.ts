1a83312fa811998e0f5d7c321b4f34162a9e10ef
Edwin Torres <edwintorres@Edwins-MacBook-Pro.local>
2022-07-30 05:13:00 -0500

    Use Value y Use Factory, con ejemplo de API_KEY pero ahora en módulo global, ejemplo database.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,11 @@ import { ProductsModule } from './products/products.module';
 import { firstValueFrom } from 'rxjs';
 import { DatabaseModule } from './database/database.module';
 
-const API_KEY = '123456';
-const API_KEY_PROD = 'PROD14213423';
-
 @Module({
   imports: [HttpModule, UsersModule, ProductsModule, DatabaseModule],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
     {
       provide: 'TASKS',
       useFactory: async (http: HttpService) => {
